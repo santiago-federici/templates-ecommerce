b99c296f95479761efa8f7b14fb0923f2987a183
santiago-federici <santiagofederici88@gmail.com>
2024-10-22 07:35:26 -0300

    update: better semantic and small changes in styles

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
+import Button from "@/components/button";
 import { Wrapper } from "@/components/wrapper";
+
 import { Template } from "@/interfaces";
 import { templates } from "@/templates";
-import Image from "next/image";
 
 export default function DetailsPage({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -24,92 +27,71 @@ export default function DetailsPage({ params }: { params: { id: string } }) {
 
   return (
     <Wrapper className="space-y-20">
-      <div className="flex gap-4">
-        <div className="space-y-6">
+      <article className="flex gap-40">
+        <div className="space-y-5">
           <p className="text-muted-foreground">{category}</p>
 
           <h2 className="text-4xl font-bold">{title}</h2>
 
           <p className="text-muted-foreground">{mediumDescription}</p>
 
-          <div className="flex gap-2">
+          <ul className="flex gap-3">
             {techStack.map((tech, index) => (
-              <div key={index} className="flex items-center gap-1">
-                <tech.icon className="h-6 w-6" />
+              <li key={index} className="flex items-center gap-1">
+                <tech.icon className="size-5" />
                 <p className="text-muted-foreground">{tech.name}</p>
-              </div>
+              </li>
             ))}
-          </div>
+          </ul>
 
-          <div className="space-x-4">
-            <button className="rounded-md bg-black px-4 py-2 font-medium text-white hover:opacity-80">
-              Live preview
-            </button>
-            <button className="rounded-md border px-4 py-2 font-medium hover:opacity-80">
-              Buy now
-            </button>
-          </div>
+          <nav className="flex items-center gap-2">
+            <Button>Live preview</Button>
+            <Button variant="outline">Buy now</Button>
+          </nav>
         </div>
 
-        <div className="w-full">
+        <figure className="w-full overflow-hidden rounded-xl shadow-lg">
           <Image
             src={image}
             alt="image one"
             width={500}
-            height={100}
-            className="h-full w-full rounded-xl shadow-lg"
+            height={700}
+            className="h-full w-full"
           />
-        </div>
-      </div>
+        </figure>
+      </article>
 
-      <div className="space-y-10">
-        <p className="text-2xl font-semibold">What&apos;s included</p>
+      <section className="space-y-10">
+        <h3 className="text-2xl font-semibold">What&apos;s included</h3>
 
         <div className="flex flex-col justify-stretch gap-4">
           {largeDescription.map((paragraph, index) => (
-            <p key={index} className="max-w-[80%] text-muted-foreground">
+            <p key={index} className="max-w-full text-muted-foreground">
               {paragraph}
             </p>
           ))}
         </div>
-      </div>
+      </section>
 
       <span className="h-px w-full bg-gray-300"></span>
 
-      <div className="flex gap-80">
-        <p className="text-2xl font-semibold">Screenshots</p>
+      <section className="space-y-10">
+        <h3 className="text-2xl font-semibold">Screenshots</h3>
 
-        <div className="grid grid-cols-2 gap-6">
-          <Image
-            src={screenshots.one}
-            alt="Screenshot one"
-            height={500}
-            width={500}
-            className="h-auto w-full rounded-md border"
-          />
-          <Image
-            src={screenshots.two}
-            alt="Screenshot two"
-            height={500}
-            width={500}
-            className="h-auto w-full rounded-md border"
-          />
-          <Image
-            src={screenshots.three}
-            alt="Screenshot three"
-            height={500}
-            width={500}
-            className="h-auto w-full rounded-md border"
-          />
-          <Image
-            src={screenshots.four}
-            alt="Screenshot four"
-            height={500}
-            width={500}
-            className="h-auto w-full rounded-md border"
-          />
+        <div className="flex gap-6">
+          {Object.values(screenshots).map((screenshot, index) => (
+            <figure key={index} className="overflow-hidden rounded-md border">
+              <Image
+                src={screenshot}
+                alt={`Screenshot ${index}`}
+                height={500}
+                width={500}
+                className="h-auto w-full"
+              />
+            </figure>
+          ))}
         </div>
-      </div>
+      </section>
     </Wrapper>
   );
 }
