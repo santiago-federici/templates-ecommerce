1432a9ecabf493c9ccf7a6d453ed81b8d94e7a1a
santiago-federici <santiagofederici88@gmail.com>
2024-10-22 06:56:58 -0300

    fix: use the new price structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,20 +6,22 @@ export function Card({ template }: { template: Template }) {
 
   return (
     <article>
-      <a href={`/details/${id}`} className="flex gap-6 justify-between">
-        <div className="space-y-4 max-w-72 py-4">
+      <a href={`/details/${id}`} className="flex justify-between gap-6">
+        <div className="max-w-72 space-y-4 py-4">
           <div>
-            <h3 className="font-semibold text-lg">{title}</h3>
+            <h3 className="text-lg font-semibold">{title}</h3>
             <p className="text-muted-foreground">{category}</p>
           </div>
 
           <p className="text-muted-foreground">{shortDescription}</p>
 
-          <span className="w-10 h-px bg-gray-300"></span>
+          <span className="h-px w-10 bg-gray-300"></span>
 
           <p className="text-muted-foreground">
-            <span className="font-medium text-foreground">${price} </span>or
-            included with all-access
+            <span className="font-medium text-foreground">
+              ${price.amount}{" "}
+            </span>
+            or included with all-access
           </p>
         </div>
 
@@ -29,21 +31,21 @@ export function Card({ template }: { template: Template }) {
             alt="Thumbnail one"
             width={288}
             height={192}
-            className="rounded-md w-72 h-48"
+            className="h-48 w-72 rounded-md"
           />
           <Image
             src={thumbnails.two}
             alt="Thumbnail two"
             width={288}
             height={192}
-            className="rounded-md w-72 h-48"
+            className="h-48 w-72 rounded-md"
           />
           <Image
             src={thumbnails.three}
             alt="Thumbnail three"
             width={288}
             height={192}
-            className="rounded-md w-72 h-48"
+            className="h-48 w-72 rounded-md"
           />
         </div>
       </a>
