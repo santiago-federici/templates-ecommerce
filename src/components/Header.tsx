42c625de0d40408108b8b5159da304f06b69ab94
santiago-federici <santiagofederici88@gmail.com>
2024-10-22 09:16:06 -0300

    update: responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,58 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+
+import { MenuIcon, XIcon } from "./icons";
+import Button from "./button";
 
 export function Header() {
-  return (
-    <header className="flex justify-between items-center h-20 wrapper">
-      <div className="bg-neutral-500 rounded-sm w-40 h-8"></div>
+  const [open, setOpen] = useState<boolean>(false);
 
-      <nav className="flex gap-10 items-center">
+  return (
+    <header className="wrapper flex h-20 items-center justify-between">
+      <div className="h-8 w-40 rounded-sm bg-neutral-500"></div>
+
+      <span onClick={() => setOpen((prev) => !prev)}>
+        <MenuIcon className="size-6 cursor-pointer lg:hidden" />
+      </span>
+
+      {open && (
+        <>
+          <div className="absolute inset-0 z-10 bg-black/40"></div>
+          <aside className="absolute right-0 top-0 z-30 h-screen w-72 space-y-10 overflow-hidden bg-background px-6 py-7 shadow-2xl md:w-96 md:px-14">
+            <span onClick={() => setOpen((prev) => !prev)}>
+              <XIcon className="ml-auto size-6 cursor-pointer" />
+            </span>
+
+            <nav>
+              <ul className="space-y-4 text-lg font-medium">
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="/templates">Templates</Link>
+                </li>
+                <li>
+                  <Link href="/pricing">Pricing</Link>
+                </li>
+                <li>
+                  <Link href="/docs">Docs</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <nav className="flex flex-col gap-2">
+              <Button size="large" variant="secondary">
+                Sign in
+              </Button>
+              <Button size="large">Get full access</Button>
+            </nav>
+          </aside>
+        </>
+      )}
+
+      <nav className="hidden items-center gap-10 lg:flex">
         <ul className="flex gap-4">
           <li>
             <Link href="/">Home</Link>
@@ -23,7 +70,7 @@ export function Header() {
         <span className="h-6 w-px bg-gray-300"></span>
         <div className="flex gap-4">
           <button>Sign In</button>
-          <button className="rounded-md bg-black text-white px-4 py-2 font-medium hover:opacity-80 duration-200">
+          <button className="rounded-md bg-black px-4 py-2 font-medium text-white duration-200 hover:opacity-80">
             Get full access
           </button>
         </div>
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -170,3 +170,40 @@ export const AstroIcon = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
+
+export const MenuIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 6l16 0" />
+    <path d="M4 12l16 0" />
+    <path d="M4 18l16 0" />
+  </svg>
+);
+
+export const XIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M18 6l-12 12" />
+    <path d="M6 6l12 12" />
+  </svg>
+);
