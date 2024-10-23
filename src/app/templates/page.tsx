3a250f4703ec39def16e5818b6e700f29e79ff7b
santiago-federici <santiagofederici88@gmail.com>
2024-10-23 12:58:40 -0300

    fix: add key to the list

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,15 +1,16 @@
 import { Wrapper } from "@/components/wrapper";
 import { Card } from "@/components/card";
 import { templates } from "@/templates";
+import React from "react";
 
 export default async function TemplatesPage() {
   return (
     <Wrapper className="md:gap-20">
       {templates.map((template) => (
-        <>
-          <Card key={template.id} template={template} />
+        <React.Fragment key={template.id}>
+          <Card template={template} />
           <div className="my-8 h-px w-full bg-gray-300 last:hidden md:hidden" />
-        </>
+        </React.Fragment>
       ))}
     </Wrapper>
   );
