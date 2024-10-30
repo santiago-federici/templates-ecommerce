17088a041782dd608862e4da2ee316c25dffd266
santiago-federici <santiagofederici88@gmail.com>
2024-10-30 12:03:36 -0300

    update: modify the cards layout

--- src/components/home-page/best-sellers-section.tsx
+++ src/components/home-page/best-sellers-section.tsx
@@ -14,7 +14,7 @@ export default function BestSellersSection() {
           Best <span className="text-primary">sellers</span>
         </h2>
 
-        <div className="flex gap-4">
+        <div className="mx-auto grid w-fit gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:w-full xl:grid-cols-templates">
           {templates.map((template) => (
             <Fragment key={template.id}>
               <Card template={template} />
