3b5a336e74de49952940de07b9883b0ab50426ec
santiago-federici <santiagofederici88@gmail.com>
2024-10-30 12:03:12 -0300

    update: remove unused cn function

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -4,16 +4,11 @@ import Wrapper from "@/components/wrapper";
 import { Card } from "@/components/card";
 
 import { templates } from "@/templates";
-import { cn } from "@/lib/utils";
 
 export default async function TemplatesPage() {
   return (
     <Wrapper>
-      <div
-        className={cn(
-          "mx-auto grid w-fit grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:w-full xl:grid-cols-templates",
-        )}
-      >
+      <div className="mx-auto grid w-fit grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:w-full xl:grid-cols-templates">
         {templates.map((template) => (
           <React.Fragment key={template.id}>
             <Card template={template} />
