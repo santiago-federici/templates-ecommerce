6d5639eb860b288b96ba536fa976f062fbdadcb8
santiago-federici <santiagofederici88@gmail.com>
2024-10-30 12:38:32 -0300

    feat: create how it works section. Create steps component. Add svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/home-page/hero-section";
 import FeaturesSection from "@/components/home-page/features-section";
 import Waves from "@/components/waves";
 import BestSellersSection from "@/components/home-page/best-sellers-section";
+import HowItWorksSection from "@/components/home-page/how-it-works-section";
 
 export default async function HomePage() {
   return (
@@ -13,6 +14,8 @@ export default async function HomePage() {
       <FeaturesSection />
 
       <BestSellersSection />
+
+      <HowItWorksSection />
     </>
   );
 }
---
+++ src/components/home-page/how-it-works-section.tsx
@@ -0,0 +1,66 @@
+import { Fragment } from "react";
+import { HowItWorksIcon } from "../icons";
+import StepComponent from "./steps-component";
+
+const steps = [
+  {
+    number: 1,
+    title: "Choose your template",
+    description:
+      "Explore our collections to find the perfect fit for your needs.",
+  },
+  {
+    number: 2,
+    title: "Purchase Your Template",
+    description: "Complete your purchase in just a few clicks.",
+  },
+  {
+    number: 3,
+    title: "Download and Install",
+    description:
+      "Download the files and follow the quick setup to install the template.",
+  },
+  {
+    number: 4,
+    title: "Customize to Your Style",
+    description: "The template is fully editable.",
+  },
+  {
+    number: 5,
+    title: "Ongoing Support and Updates",
+    description: "Constant updates to keep your design fresh and optimized.",
+  },
+];
+
+export default function HowItWorksSection() {
+  return (
+    <section className="relative z-10">
+      <div className="absolute -z-10 size-full bg-[#6CA7D3]"></div>
+
+      <div className="wrapper flex items-center justify-center gap-10 py-20 lg:py-0">
+        <div className="w-3/4 max-lg:hidden xl:w-full">
+          <HowItWorksIcon className="mx-auto w-full xl:w-2/3" />
+        </div>
+        <div className="w-full text-white">
+          <h2 className="text-2xl font-semibold">How it works</h2>
+          <p className="text-white/80">
+            Follow the step to get your template ready and running
+          </p>
+
+          <div className="mt-8 max-lg:space-y-4">
+            {steps.map((step) => (
+              <Fragment key={step.number}>
+                <StepComponent
+                  number={step.number}
+                  title={step.title}
+                  description={step.description}
+                />
+                <div className="ml-5 h-4 w-px bg-white/30 last:hidden max-lg:hidden"></div>
+              </Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
