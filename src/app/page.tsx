23cde2b700f34527e4f466b50ae57dde163b666f
santiago-federici <santiagofederici88@gmail.com>
2024-10-30 08:36:41 -0300

    fix: horizontal scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Waves from "@/components/waves";
 export default async function HomePage() {
   return (
     <>
-      <div className="absolute top-0 -z-10 h-screen w-screen bg-primary"></div>
+      <div className="absolute top-0 -z-10 h-screen w-full bg-primary"></div>
 
       <HeroSection />
 
