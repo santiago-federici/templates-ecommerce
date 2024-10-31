6ffae95a1ff57fb860208076bc14faa7186d9818
santiago-federici <santiagofederici88@gmail.com>
2024-10-31 10:52:33 -0300

    feat: create mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,36 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect } from "react";
 
 import Button from "./button";
 
 import { cn } from "@/lib/utils";
 
 import { NAVIGATION } from "@/constants/navigation";
-import { MenuIcon, XIcon } from "./icons";
+
+import { AnimatePresence, motion, MotionConfig, useCycle } from "framer-motion";
 
 export function Header() {
-  const [open, setOpen] = useState<boolean>(false);
+  const [menu, toggleMenu] = useCycle(false, true);
   const pathname = usePathname();
+
+  // disable scrolling when menu is open
+  useEffect(() => {
+    if (menu) {
+      setTimeout(() => (document.body.style.overflow = "hidden"), 500);
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [menu]);
 
   return (
     <header
-      className={cn("bg-primary", {
+      className={cn("relative bg-primary", {
         "bg-white": pathname !== "/",
       })}
     >
@@ -29,41 +43,6 @@ export function Header() {
         >
           Ecommerce
         </h1>
-
-        <span onClick={() => setOpen((prev) => !prev)}>
-          <MenuIcon className="size-6 cursor-pointer lg:hidden" />
-        </span>
-
-        {open && (
-          <>
-            <div className="absolute inset-0 z-10 bg-black/40"></div>
-            <aside className="absolute right-0 top-0 z-30 h-screen w-72 space-y-10 overflow-hidden bg-background px-6 py-7 shadow-2xl md:w-96 md:px-14">
-              <span onClick={() => setOpen((prev) => !prev)}>
-                <XIcon className="ml-auto size-6 cursor-pointer" />
-              </span>
-
-              <nav>
-                <ul className="space-y-4 text-lg font-medium">
-                  {NAVIGATION.map((link) => (
-                    <li
-                      key={link.href}
-                      className="duration-200 hover:text-muted-foreground"
-                    >
-                      <Link href={link.href}>{link.name}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-
-              <nav className="flex flex-col gap-2">
-                <Button size="large" variant="secondary">
-                  Sign in
-                </Button>
-                <Button size="large">Get full access</Button>
-              </nav>
-            </aside>
-          </>
-        )}
 
         <nav className="hidden items-center gap-10 lg:flex">
           <ul className="flex gap-4">
@@ -96,6 +75,104 @@ export function Header() {
             <Button>Get full access</Button>
           </div>
         </nav>
+
+        <motion.button
+          aria-label="Icono de menú"
+          animate={menu ? "open" : "close"}
+          className={cn(
+            "z-50 flex flex-col gap-y-1 px-1 py-1.5 lg:hidden [&>span]:bg-white",
+            {
+              "right-6 top-6 max-md:fixed": menu,
+              "[&>span]:bg-black": pathname !== "/" || menu,
+            },
+          )}
+          onClick={() => toggleMenu()}
+        >
+          <motion.span
+            variants={{
+              open: {
+                rotate: 45,
+                y: 6,
+              },
+              close: {
+                rotate: 0,
+                y: 0,
+              },
+            }}
+            className="h-0.5 w-5"
+          ></motion.span>
+          <motion.span
+            variants={{
+              open: {
+                rotate: 45,
+                opacity: 0,
+              },
+              close: {
+                rotate: 0,
+                opacity: 1,
+              },
+            }}
+            className="h-0.5 w-5"
+          ></motion.span>
+          <motion.span
+            variants={{
+              open: {
+                rotate: -45,
+                y: -6,
+              },
+              close: {
+                rotate: 0,
+                y: 0,
+              },
+            }}
+            className="h-0.5 w-5"
+          ></motion.span>
+        </motion.button>
+
+        <AnimatePresence>
+          {menu && (
+            <MotionConfig
+              transition={{
+                type: "spring",
+                bounce: 0,
+              }}
+            >
+              <motion.aside
+                variants={{
+                  initial: {
+                    x: "100%",
+                  },
+                  animate: {
+                    x: "0",
+                  },
+                }}
+                initial="initial"
+                animate="animate"
+                exit="initial"
+                className="fixed inset-0 z-40 flex flex-col gap-4 bg-background px-6 py-7 shadow-lg md:px-16 lg:hidden"
+              >
+                <nav className="mt-8 flex flex-col items-start gap-6 lg:hidden">
+                  {NAVIGATION.map((link) => (
+                    <li
+                      key={link.href}
+                      className={cn(
+                        "text-xl text-muted-foreground duration-200 hover:text-black",
+                        {
+                          "text-black": pathname === link.href,
+                        },
+                      )}
+                      onClick={() => {
+                        setTimeout(() => toggleMenu(), 500);
+                      }}
+                    >
+                      <Link href={link.href}>{link.name}</Link>
+                    </li>
+                  ))}
+                </nav>
+              </motion.aside>
+            </MotionConfig>
+          )}
+        </AnimatePresence>
       </div>
     </header>
   );
