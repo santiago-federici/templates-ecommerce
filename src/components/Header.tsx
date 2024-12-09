4770b827fc6424b436b9676e9268c8f7ce4333ce
santiago-federici <santiagofederici88@gmail.com>
2024-12-09 09:27:33 -0300

    fix: remove unnecessary div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,36 +30,34 @@ export function Header() {
   }, [menu]);
 
   return (
-    <header className="relative">
-      <div className="wrapper flex h-20 items-center justify-between">
-        <h1 className="text-4xl font-black tracking-tighter text-foreground">
-          Ecommerce
-        </h1>
+    <header className="wrapper flex h-20 items-center justify-between">
+      <h1 className="text-4xl font-black tracking-tighter text-foreground">
+        Ecommerce
+      </h1>
 
-        <ThemeToggle />
+      <ThemeToggle />
 
-        <nav className="hidden items-center lg:flex">
-          <ul className="flex gap-x-4">
-            {NAVIGATION.map((link) => (
-              <li
-                key={link.href}
-                className="text-muted-foreground duration-100 hover:text-foreground"
-              >
-                <Link href={link.href}>{link.name}</Link>
-              </li>
-            ))}
-          </ul>
+      <nav className="hidden items-center lg:flex">
+        <ul className="flex gap-x-4">
+          {NAVIGATION.map((link) => (
+            <li
+              key={link.href}
+              className="text-muted-foreground duration-100 hover:text-foreground"
+            >
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
 
-          <span className="ml-5 mr-2 h-8 w-px bg-gray-300"></span>
+        <span className="ml-5 mr-2 h-8 w-px bg-gray-300"></span>
 
-          <div className="flex gap-2">
-            <Button variant="ghost">Sign In</Button>
-            <Button>Get full access</Button>
-          </div>
-        </nav>
+        <div className="flex gap-2">
+          <Button variant="ghost">Sign In</Button>
+          <Button>Get full access</Button>
+        </div>
+      </nav>
 
-        <MobileMenu menu={menu} pathname={pathname} toggleMenu={toggleMenu} />
-      </div>
+      <MobileMenu menu={menu} pathname={pathname} toggleMenu={toggleMenu} />
     </header>
   );
 }
