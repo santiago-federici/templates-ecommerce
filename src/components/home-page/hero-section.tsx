3d124325d42d1aafaed344f16fa7655a4fbd93de
santiago-federici <santiagofederici88@gmail.com>
2024-12-12 12:11:14 -0300

    refactor: change hero section image

--- src/components/home-page/hero-section.tsx
+++ src/components/home-page/hero-section.tsx
@@ -1,20 +1,22 @@
 import Wrapper from "../wrapper";
-import TemporarySVG from "../temporary-svg";
 
 export default function HeroSection() {
   return (
-    <Wrapper className="flex h-[calc(100vh-80px)] flex-col items-center justify-between gap-20 lg:flex-row">
-      <div className="space-y-2 max-lg:text-center">
-        <h1 className="font-thunder text-6xl md:text-8xl">
+    <Wrapper className="flex min-h-[65svh] flex-col gap-20 max-lg:mt-20 lg:flex-row lg:items-center lg:justify-between">
+      <div className="w-full max-lg:text-center">
+        <h1 className="font-thunder text-6xl lg:max-w-[25ch] 2xl:text-8xl">
           SOME BIG ASS TITLE THAT WRAPS LIKE THIS
         </h1>
-        <p className="text-lg text-muted-foreground md:text-xl">
+        <p className="text-lg text-muted-foreground lg:max-w-[50ch] xl:text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex possimus
           doloremque eligendi ab alias placeat provident, repellat sunt. Ipsam,
           veniam? Deleniti explicabo at sit in nobis facere ipsam numquam totam.
         </p>
       </div>
-      <TemporarySVG />
+
+      <div className="h-auto w-full">
+        <img src="/hero.webp" alt="test" className="h-full w-full bg-cover" />
+      </div>
     </Wrapper>
   );
 }
