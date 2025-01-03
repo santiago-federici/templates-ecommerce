68495df897c87a8accca6f9d97794b802d615e6e
santiago-federici <santiagofederici88@gmail.com>
2025-01-03 17:16:46 -0300

    feat: add divider between cards

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -7,21 +7,15 @@ import { templates } from "@/templates";
 export default async function TemplatesPage() {
   return (
     <Wrapper className="py-20">
-      <section className="flex flex-col gap-16">
+      <section className="flex flex-col gap-20">
         {templates.map((template) => (
           <Fragment key={template.id}>
             <TemplateCard template={template} />
+
+            <span className="mx-auto h-px w-[90%] bg-black/10 last:hidden"></span>
           </Fragment>
         ))}
       </section>
-
-      {/* <div className="mx-auto grid w-full grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-templates">
-        {templates.map((template) => (
-          <Fragment key={template.id}>
-            <Card template={template} />
-          </Fragment>
-        ))}
-      </div> */}
     </Wrapper>
   );
 }
