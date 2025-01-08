b42027d9ef8be53abf8189282ff6da7db09408fe
santiago-federici <santiagofederici88@gmail.com>
2025-01-08 08:05:00 -0300

    refactor: adjust card's images' sizes

--- src/components/template-card.tsx
+++ src/components/template-card.tsx
@@ -12,47 +12,41 @@ export default function TemplateCard({ template }: { template: Template }) {
     <article>
       <a
         href={`/details/${id}`}
-        className="flex justify-between gap-8 max-md:flex-col md:h-44"
+        className="flex justify-between gap-4 max-md:flex-col md:h-48"
       >
-        <div className="flex w-full max-w-[40ch] flex-col gap-4">
-          <h3 className="text-xl font-semibold">
-            {title}{" "}
-            <span className="text-base font-normal text-muted-foreground">
-              - {category}
-            </span>
-          </h3>
-
-          <p className="text-muted-foreground">{shortDescription}</p>
-
-          <p className="font-semibold">
-            ${price.amount}{" "}
-            <span className="font-normal text-muted-foreground">
-              - or included with all-access
-            </span>
+        <div className="w-full md:w-fit md:max-w-[30ch]">
+          <p className="mb-2 text-sm font-normal text-muted-foreground">
+            {category}
           </p>
+          <h3 className="mb-4 text-xl font-semibold">{title}</h3>
+
+          <p className="mb-4 text-muted-foreground">{shortDescription}</p>
+
+          <p className="font-semibold">${price.amount}</p>
         </div>
 
-        <div className="flex w-fit gap-4 overflow-x-auto [scrollbar-color:#888_transparent;] [scrollbar-width:thin]">
+        <div className="flex w-full gap-4 overflow-x-auto [scrollbar-color:#888_transparent;] [scrollbar-width:thin]">
           <Image
             src={one}
             alt="Screenshot one"
             width={1607}
             height={904}
-            className="h-auto w-full rounded-xl md:h-full md:w-auto"
+            className="h-auto w-full rounded-xl"
           />
           <Image
             src={two}
             alt="Screenshot two"
             width={1607}
             height={904}
-            className="hidden h-full w-auto rounded-xl md:block"
+            className="hidden h-auto w-full rounded-xl md:block"
           />
+
           <Image
             src={three}
             alt="Screenshot three"
             width={1607}
             height={904}
-            className="hidden h-full w-auto rounded-xl md:block"
+            className="hidden h-auto w-full rounded-xl md:block"
           />
         </div>
       </a>
