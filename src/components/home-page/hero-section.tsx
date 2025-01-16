387010e0ce8a71d1991865ade1f2d08e3423df6f
santiago-federici <santiagofederici88@gmail.com>
2025-01-16 20:47:47 -0300

    refactor: change the title and description

--- src/components/home-page/hero-section.tsx
+++ src/components/home-page/hero-section.tsx
@@ -5,12 +5,15 @@ export default function HeroSection() {
     <Wrapper className="mb-40 flex min-h-[65svh] flex-col gap-20 max-lg:mt-10 lg:flex-row lg:items-center lg:justify-between">
       <div className="w-full space-y-6 max-lg:text-center">
         <h1 className="font-anton text-6xl font-black lg:max-w-[25ch] 2xl:text-8xl">
-          SOME BIG ASS TITLE THAT WRAPS LIKE THIS
+          CRAFT STUNNING WEBSITES{" "}
+          <span className="bg-gradient-to-b from-black to-primary bg-clip-text text-transparent">
+            HASSLE-FREE
+          </span>
         </h1>
         <p className="text-lg text-muted-foreground lg:max-w-[50ch] xl:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex possimus
-          doloremque eligendi ab alias placeat provident, repellat sunt. Ipsam,
-          veniam? Deleniti explicabo at sit in nobis facere ipsam numquam totam.
+          Discover a vast selection of premium templates tailored for any
+          project. Create professional, eye-catching websites effortlessly with
+          tools designed to save you time and boost creativity.
         </p>
       </div>
 
